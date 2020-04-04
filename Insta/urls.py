7317002a693a55959688dc35db29e3f5e19f1739
Zhou <zhouy2@ee-126588.attlocal.net>
2020-04-04 00:34:23 -0500

    display static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import include, path
 
+from Insta.views import HelloDjango
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('insta/', include('Insta.urls')),
+    path('', HelloDjango.as_view(), name='helloDjango')
 ]
---
+++ Insta/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+class HelloDjango(TemplateView):
+    template_name = 'home.html'
+
+# Create your views here.
